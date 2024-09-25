207ba9c9651ae5a74e5da263925c65815a5a16b2
thincoffeedev <thincoffeedev@gmail.com>
2024-09-25 15:25:56 +0700

    Hoàn thành chức năng đăng nhập và đăng xuất

---
+++ TCD_Stadium_Management/TCD_Stadium_Management/Functs/FrmLoginDao.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TCD_Stadium_Management.Models;
+
+namespace TCD_Stadium_Management.Functs
+{
+    public class FrmLoginDao
+    {
+        private readonly TCD_Stadium_ManagementEntities _db;
+        public FrmLoginDao()
+        {
+            _db = new TCD_Stadium_ManagementEntities();
+        }
+        public bool checkLogin(string username, string password)
+        {
+            try
+            {
+                var _user = _db.Users.SingleOrDefault(u=>u.Username == username && u.Password == password);
+                if (_user != null)
+                {
+                    return true;
+                }
+                return false;
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Lỗi truy cập cơ sở dữ liệu:\n" + ex.Message, "Database Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            return false;
+        }
+
+    }
+}
